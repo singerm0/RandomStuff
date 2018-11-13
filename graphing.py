796e059864cb2f7960ceeeb123ed6b81be54cba2
singerm0 <35753471+singerm0@users.noreply.github.com>
2018-11-13 16:04:46 -0500

    fixed bug

--- graphing.py
+++ graphing.py
@@ -3,154 +3,120 @@
 Created on Wed Nov  7 20:36:52 2018
 
 @author: Matthew
+ 
+creates multicolored hover over movable graph with bokeh,
+make sure to change the syspath for autoguru and the model location on your run
 """
+
 import numpy as np
 import pandas as pd
-
-import random
 from sklearn.manifold import TSNE
-from sklearn import preprocessing
-from matplotlib import pyplot as plt
-from bokeh.plotting import figure, output_file, show, ColumnDataSource
+import bokeh.plotting as bk
+from bokeh.models import HoverTool
+from copy import deepcopy
+import json
+import random
+from itertools import compress
+import collections
 
-#initializing dummy input
-def main(fake,*data):
-    if fake == True:
-        vects = [0]*500
-        questions = ['']*500
-        answers = ['']*500
-        
-        nouns = ("puppy", "car", "rabbit", "girl", "monkey")
-        verbs = ("runs", "hits", "jumps", "drives", "barfs") 
-        adv = ("crazily.", "dutifully.", "foolishly.", "merrily.", "occasionally.")
-        adj = ("adorable", "clueless", "dirty", "odd", "stupid")
-        
-        
-        for x in range(len(vects)):
-            num = random.sample(range(1,5),4)
-            vects[x] = random.sample(range(1,1000),300)
-            questions[x] = nouns[num[0]] +' '+verbs[num[1]]+' '+adv[num[2]]+' '+adj[num[3]]
-            num = random.sample(range(1,5),4)
-            answers[x] = nouns[num[0]] +' '+verbs[num[1]]+' '+adv[num[2]]+' '+adj[num[3]]
-        
 
-        startquest =  dict(zip(questions,vects))
-        startanswer = dict(zip(answers,vects))
-    
-        test = {'questions':startquest,
-                'answers':startanswer}
-    
-    questData = test['questions']
-    ansData = test['answers']
-    
-    questWord = list(questData.keys())
-    questVects = list(questData.values())
-    questionArr = npectY':tsne_results[:len(questVects),1]})
-        
-    .array(questVects)
-    
-    ansWord = list(ansData.keys())
-    ansVects = list(ansData.values())
-    answersArr = np.array(ansVects)
-    combinedVects = np.concatenate([questionArr,answersArr],axis=0)
-    tsne = TSNE(n_components =2,verbose=0,perplexity=15,n_iter=50000, early_exaggeration =5)
-    tsne_results = tsne.fit_transform(combinedVects)
-    
-    plt.scatter(tsne_results[:,0],tsne_results[:,1])
-    
-    finalQuestions = pd.DataFrame({'words':questWord,
-                                   'origVects':questVects,
-                                   'vectX':tsne_results[:len(questVects),0],
-                                   'v
-    finalAnswers = pd.DataFrame({'words':ansWord,
-                                 'origVects':ansVects,
-                                 'vectX':tsne_results[len(questVects):,0],
-                                 'vectY':tsne_results[len(questVects):,1]})
-    return (finalQuestions,finalAnswers)
+import sys
+# manually adds auto guru to my python path
+sys.path.append(r'E:\hackathon\autoguru\question-answering')
+from questionanswering.embeddings import Embedder
+#change this for your program
 
+def main():
+    model = 'E:\\hackathon\\answer\\embedder.npz'
+    # eembedder model location, change for yourprogram
+    embedder = Embedder.load(model)
+    
+    with open('E://hackathon//final//question-answers.json') as f:
+        pracDic = collections.OrderedDict(json.load(f))
+    # load json dict    
+    embedded = np.zeros((len(pracDic),300))
+    ind = 0
+    question = []
+    answers = []
+    # initialize data structures
+    for x in pracDic.items():
+        question.append(x[0])
+        answers.append(x[1])
+        embedded[ind,:] = embedder.embed(x[0])
+        ind += 1
+    #construct structures from dictionary
+    
+    embedded = pd.DataFrame(embedded) # make embedded a dataframe
+    
+    # not needed, nothing should be null
+    #pracDic = dict(compress(list(pracDic.items()),list(~embedded.isnull().any(axis=1))))
+    #embedded = embedded.loc[~embedded.isnull().any(axis=1),:]
+    
+    tsne = TSNE(n_components =2,verbose=0,perplexity=12,n_iter=10000, early_exaggeration =15)
+    tsne_results = np.array(tsne.fit_transform(embedded))
+    # creates tsne model and fits our data to it
+    finalQuest = pd.DataFrame({'quest':question,'ans':answers,'origVects':np.array(embedded).tolist(),
+                               'vectX':tsne_results[:,0],'vectY':tsne_results[:,1]})
+    df = deepcopy(finalQuest)
+    # this isnt needed i didnt want to mess with finalQuest while debugging
+        
+    def splitFrame(df):
+        ''' created a list of dataframes with each df having different questions'''
+        ansSet = list(set(df['ans']))
+        dfList = []
+        for x in ansSet:
+            manipDf = df.loc[df['ans'] == x,:]
+            dfList.append(manipDf)            
+        return(dfList)
+    splits = splitFrame(df)
+    
+    bk.output_file("toolbar.html")
+    #sets the output url    
+    TOOLTIPS = ''' 
+        <div>
+            <div> 
+                <span style="font-size: 17px; font-weight: bold;">Question:</span> 
+            </div>
+            <div> 
+                <span style="font-size: 17px;">@quest{safe}</span> 
+            </div>
+            <div> 
+                <span style="font-size: 17px; font-weight: bold;">Answer:</span> 
+            </div>
+            <div> 
+                <span style="font-size: 17px;">@ans{safe}</span> 
+            </div>
+        </div>'''
+    #creates the hover over text
+    
+    p = bk.figure(plot_width=850, plot_height=700,title='Questions and Answers')
+    p.title.text_color = 'black'
+    p.title.text_font = 'helvetica'
+    p.title.text_font_size = '24pt'
+    p.background_fill_color = '#f4f3ef'
+    p.xaxis.visible =False
+    p.yaxis.visible = False
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+    #sets details for graph
+    
+    
+    color = []
+    for x in range(1000):
+        color.append("#%06x" % random.randint(0, 0xFFFFFF))
+    colors = list(set(color))
+    #generates a list of colors
+    
+    # manually selecting colors, this is just for hte demo, comment out this line if using more
+    #colors = ['firebrick','royalblue','peru','teal','seagreen','darkmagenta','gold','black','orange']
+    
+    for x in range(len(splits)):
+        r = p.scatter('vectX','vectY', size=10,source=bk.ColumnDataSource(splits[x]),color=colors[x])
+        p.add_tools(HoverTool(renderers=[r], tooltips=TOOLTIPS))
+        #plots all of the different colored points with their respective hover text
+        
+    bk.show(p)
+    return (splits,pracDic)
+    #shows the plot
 if __name__ == '__main__':
-    (questions,answers) = main(1)
-
-#%%
-
-output_file("toolbar.html")
-souce = ColumnDataSource(data=dict(
-        x = list(questions.vectX),
-        y = list(questions.vectY),
-        words = list(questions.words)
-        ))
-TOOLTIPS = '''
-    <div>
-        <span style="font-size: 17px; font-weight: bold;">@desc</span>
-    </div> '''
-# create a new plot with the toolbar below
-bplt = figure(plot_width=1000, plot_height=1000, tooltips=TOOLTIPS,
-           title=None, toolbar_location='below')
-bplt.circle('x','y', size=10,souce=souce)
-
-show(bplt) 
-#%%
-from bokeh.plotting import figure, output_file, show
-
-output_file("toolbar.html")
-
-# create a new plot with the toolbar below
-p = figure(plot_width=400, plot_height=400,
-           title=None, toolbar_location="below")
-
-p.circle([1, 2, 3, 4, 5], [2, 5, 8, 2, 7], size=10)
-
-show(p)  
-
-#%%
-from bokeh.plotting import figure, output_file, show, ColumnDataSource
-
-output_file("toolbar.html")
-
-source = ColumnDataSource(data=dict(
-    x=[1, 2, 3, 4, 5],
-    y=[2, 5, 8, 2, 7],
-    desc=['A', 'b', 'C', 'd', 'E'],
-    imgs=[
-        'https://bokeh.pydata.org/static/snake.jpg',
-        'https://bokeh.pydata.org/static/snake2.png',
-        'https://bokeh.pydata.org/static/snake3D.png',
-        'https://bokeh.pydata.org/static/snake4_TheRevenge.png',
-        'https://bokeh.pydata.org/static/snakebite.jpg'
-    ],
-    fonts=[
-        '<i>italics</i>',
-        '<pre>pre</pre>',
-        '<b>bold</b>',
-        '<small>small</small>',
-        '<del>del</del>'
-    ]
-))
-
-TOOLTIPS = """
-    <div>
-        <div>
-            <img
-                src="@imgs" height="42" alt="@imgs" width="42"
-                style="float: left; margin: 0px 15px 15px 0px;"
-                border="2"
-            ></img>
-        </div>
-        <div>
-            <span style="font-size: 17px; font-weight: bold;">@desc</span>
-            <span style="font-size: 15px; color: #966;">[$index]</span>
-        </div>
-        <div>
-            <span>@fonts{safe}</span>
-        </div>
-        <div>
-            <span style="font-size: 15px;">Location</span>
-            <span style="font-size: 10px; color: #696;">($x, $y)</span>
-        </div>
-    </div>
-"""
-#%%
-p = figure(plot_width=400, plot_height=400, tooltips=TOOLTIPS,
-           title="Mouse over the dots")
-
-p.circle('x', 'y', size=20, source=source)
+    (splits,pracDic) = main()
